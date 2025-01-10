4daed8767ab8e4f6166a78e9ac0727679de1f967
Patryk Majdak <pmajdak54@gmail.com>
2025-01-10 11:51:51 +0100

    Lab-3

---
+++ Simulator/DirectionParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+public static class DirectionParser
+{
+    public static Direction[] Parse(string movementString)
+    {
+        List<Direction> directions = new List<Direction>();
+        foreach (char c in movementString.ToUpper())
+        {
+            switch (c)
+            {
+                case 'U':
+                    directions.Add(Direction.Up);
+                    break;
+                case 'R':
+                    directions.Add(Direction.Right);
+                    break;
+                case 'D':
+                    directions.Add(Direction.Down);
+                    break;
+                case 'L':
+                    directions.Add(Direction.Left);
+                    break;
+            }
+        }
+        return directions.ToArray();
+    }
+}
