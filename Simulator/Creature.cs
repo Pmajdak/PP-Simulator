6e7f12937cf4e6ff05570ecf52f9e35f5b4d2be6
Patryk Majdak <pmajdak54@gmail.com>
2025-01-10 11:33:26 +0100

    Settery

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -4,28 +4,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Simulator
+namespace Simulator;
+
+public class Creature
 {
-    public class Creature
+    private string name = "Unknown";
+    private int level = 1;
+
+    public string Name
     {
-        public string Name { get; set; }
-        public int Level { get; set; }
-
-        public Creature(string name, int level = 1)
+        get { return name; }
+        init
         {
-            Name = name;
-            Level = level;
+            if (value.Length > 25)
+            {
+                value = value.Substring(0, 25).TrimEnd();
+            }
+            value = value.Trim();
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }
+            if (char.IsLower(value[0]))
+            {
+                value = char.ToUpper(value[0]) + value.Substring(1);
+            }
+            name = value;
         }
+    }
 
-        public Creature() { }
-
-        public void SayHi()
+    public int Level
+    {
+        get { return level; }
+        init
         {
-            Console.WriteLine($"Hi, I'm {Name}!");
+            if (value < 1)
+            {
+                level = 1;
+            }
+            else if (value > 10)
+            {
+                level = 10;
+            }
+            else
+            {
+                level = value;
+            }
         }
-        public string Info
+    }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() { }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+    public string Info => $"{Name} [{Level}]";
+
+    public void Upgrade()
+    {
+        if (level < 10)
         {
-            get { return $"{Name} [{Level}]"; }
+            level++;
         }
     }
 
