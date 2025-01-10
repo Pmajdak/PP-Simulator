152ecfc63a09a2c73a1dfa73669b843948a2a91e
Patryk Majdak <pmajdak54@gmail.com>
2025-01-10 20:12:13 +0100

    Simulation

---
+++ Simulator/Simulation.cs
@@ -0,0 +1,45 @@
+﻿using Simulator.Maps;
+using Simulator;
+
+public class Simulation
+{
+    public Map Map { get; }
+    public List<Creature> Creatures { get; }
+    public List<Point> Positions { get; }
+    public string Moves { get; }
+    public bool Finished { get; private set; } = false;
+
+    private int moveIndex = 0;
+
+    public Creature CurrentCreature => Creatures[moveIndex % Creatures.Count];
+    public string CurrentMoveName => Moves[moveIndex % Moves.Length].ToString().ToLower();
+
+    public Simulation(Map map, List<Creature> creatures, List<Point> positions, string moves)
+    {
+        if (!creatures.Any())
+            throw new ArgumentException("Blad! - Lista stworow nie moze byc pusta.");
+
+        if (creatures.Count != positions.Count)
+            throw new ArgumentException("Blad! - Lista pozycji startowych musi byc rowna liscie stworow.");
+
+        Map = map;
+        Creatures = creatures;
+        Positions = positions;
+        Moves = moves;
+
+        for (int i = 0; i < creatures.Count; i++)
+        {
+            creatures[i].AssignMap(map, positions[i]);
+        }
+    }
+
+    public void Turn()
+    {
+        if (Finished) throw new InvalidOperationException("Blad! - Symulacja sie zakonczyla.");
+
+        CurrentCreature.Go(DirectionParser.Parse(Moves)[moveIndex % Moves.Length]);
+
+        if (++moveIndex >= Moves.Length * Creatures.Count)
+            Finished = true;
+    }
+}
